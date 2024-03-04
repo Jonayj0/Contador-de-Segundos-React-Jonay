cbfa35a37eb975a0cb2aab163cb6d0cd70dcaa61
Jonayj0 <141333392+Jonayj0@users.noreply.github.com>
2024-03-04 13:49:35 +0000

    React Contador Segundos Jonay

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,23 +4,22 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="d-flex justify-content-center bg-dark text-white pb-5">
+			<h1><svg xmlns ="http://www.w3.org/2000/svg" width="50" height="50" fill="white" class="bi bi-clock" viewBox="0 0 16 16">
+				<path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/>
+				<path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0"/>
+				</svg></h1>
+			<h1 className="card text-center bg-secondary mt-5 m-2 border p-4">{props.digito6}</h1>
+			<h1 className="card text-center bg-secondary mt-5 m-2 border p-4">{props.digito5}</h1>
+			<h1 className="card text-center bg-secondary mt-5 m-2 border p-4">{props.digito4}</h1>
+			<h1 className="card text-center bg-secondary mt-5 m-2 border p-4">{props.digito3}</h1>
+			<h1 className="card text-center bg-secondary mt-5 m-2 border p-4">{props.digito2}</h1>
+			<h1 className="card text-center bg-secondary mt-5 m-2 border p-4">{props.digito1}</h1>
 		</div>
 	);
 };
+
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -8,5 +8,40 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
+
+
+
+
+let num6 = 0;
+let num5 = 0;
+let num4 = 0;
+let num3 = 0;
+let num2 = 0;
+let num1 = 0;
+
+setInterval(function () {
+    // console.log(num1++, num2, num3);
+    num1++;
+
+    if (num1 === 10) {
+        num1 = 0;
+        num2++;
+    } if (num2 === 10) {
+        num2 = 0;
+        num3++;
+    } if (num3 === 10) {
+        num3 = 0;
+        num4++;
+    } if (num4 === 10) {
+        num4 = 0;
+        num5++;
+    } if (num5 === 10) {
+        num5 = 0;
+        num6++;
+    }
+
+    ReactDOM.render(<Home digito1={num1} digito2={num2} digito3={num3} digito4={num4} digito5={num5} digito6={num6}/>, document.querySelector("#app"));} , 1000);
+
+
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
